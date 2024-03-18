6b41d32027f5e370238eadc7efd8381d79e30524
Sourav Singh <sourav859987@gmail.com>
2024-03-18 21:20:51 +0530

    learn stack & heap memory and  garbage collection

--- 03-datatype-summary.js
+++ 03-datatype-summary.js
@@ -47,6 +47,36 @@ let myFunction = () => {
 // +++++++++++++++++  Stack and Heap memory ++++++++++++++
 // Stack (Primitive) , Heap (Non Primitive)
 
+// Stack Memory:
+
+/* The stack is a special region of memory that stores variables created by functions.
+When a function is called, a new stack frame is created on top of the stack, which contains the function's arguments, local variables, and any other function-specific data.
+When the function finishes executing, its stack frame is removed from the stack, and control returns to the calling function.
+Variables with primitive data types like numbers, booleans, and references to objects are stored on the stack.
+Example:
+
+function foo() {
+    let x = 10; // x is stored on the stack
+    console.log(x);
+}
+foo();
+
+
+// Heap Memory:
+
+The heap is a larger region of memory used for dynamic memory allocation.
+Objects and data structures in JavaScript are allocated memory on the heap.
+When an object is created, memory is allocated for it on the heap, and a reference to that memory location is stored on the stack or within another object.
+JavaScript engine automatically handles memory allocation and deallocation (garbage collection) for objects on the heap.
+Example:
+
+let obj = { name: "John", age: 30 }; // Object is stored on the heap, reference to object is stored on the stack
+
+// garbage collection 
+
+Garbage collection in JavaScript is an automatic memory management process carried out by JavaScript engines to reclaim memory occupied by objects that are no longer needed or referenced. The goal of garbage collection is to free up memory that is no longer in use, thereby preventing memory leaks and ensuring efficient memory usage
+*/
+
 let myYoutubeName = "sourav43";
 
 let anotherName = myYoutubeName;
