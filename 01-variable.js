a9e7cfae633b38995f452f2d5d20bd193443f48b
Sourav Singh <sourav859987@gmail.com>
2024-03-18 21:02:24 +0530

    add var example

--- 01-variable.js
+++ 01-variable.js
@@ -7,13 +7,22 @@ accountNo = 23432;
 
 let accountState;
 
-console.table([souravProfession, accountId, city, accountNo, accountState]);
+// console.table([souravProfession, accountId, city, accountNo, accountState]);
 
 // scope kya h ?  ans:  {}
 /* 
 prefer not to use var
 because of issue in block scope and functional scope
 */
+// console.warn('get a warning');
+// console.error('error occured')
 
-console.warn('get a warning');
-console.error('error occured')
+var name = 'sourav'
+
+function printName() {
+    var name = 'sumit'
+    console.log(name);
+}
+
+console.log(name);
+// printName()
