6de0c839694d74fad3819cd42b80f05d6bad5271
Sourav Singh <sourav859987@gmail.com>
2024-03-30 14:38:41 +0530

    add some details

--- projects/passwordGenerator/script.js
+++ projects/passwordGenerator/script.js
@@ -6,9 +6,7 @@ const compareBtn = document.querySelector("#compare-btn");
 generateBtn.addEventListener("click", () => {
   let minValue = 100000;
   let maxValue = 999999;
-  let randomPassword = Math.floor(
-    Math.random() * (maxValue - minValue) + minValue
-  );
+  let randomPassword = Math.floor(Math.random() * ((maxValue - minValue) + 1) + minValue);
   password.innerText = randomPassword;
 });
 
