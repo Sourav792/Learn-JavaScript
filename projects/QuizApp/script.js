4c53e82ea154dba28ad1d8d3cc4d767d064c5d27
Sourav Singh <sourav859987@gmail.com>
2024-04-11 15:24:38 +0530

    make quiz app

---
+++ projects/QuizApp/script.js
@@ -0,0 +1,18 @@
+// dark to light mode switcher
+// let body = document.querySelector("body");
+// let mode = document.querySelector("#mode");
+// let modeIcon = document.querySelector("#mode_icon");
+// mode.addEventListener("click", () => {
+//     modeIcon.classList.toggle('ri-sun-line')
+//   if (body.className == "light") {
+//       body.classList.add("dark");
+//       body.classList.remove("light");
+//   } else{
+//     body.classList.add("light");
+//     body.classList.remove("dark");
+//   }
+// });
+
+// 
+let apiURL = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple';
+console.log(apiURL);
