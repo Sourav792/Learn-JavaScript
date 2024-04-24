004160d2de0f4304b2c65c4656dd1935ce580f4d
Sourav Singh <sourav859987@gmail.com>
2024-04-24 12:00:48 +0530

    add some details

--- oop/call.js
+++ oop/call.js
@@ -1,3 +1,9 @@
+// --------------------------------- .Call method -------------------------------
+// The .call() method used to invoke a function with a specified this value and arguments provided individually.
+//  This method allows you to control the current context (the value of this) in which a function is executed. 
+
+// functionName.call(thisArg, arg1, arg2, ...args)
+
 function setUserName(username) {
     // complex DB calls
     this.username = username
@@ -7,7 +13,7 @@ function setUserName(username) {
 function createUser(username, email, password) {
     // setUserName(username)
     // setUserName.call(username) // reference hold
-    setUserName.call(this, username) 
+    setUserName.call(this, username) // this argument is optional
 
     this.email = email
     this.password = password
